c8793a958420d5982191861d5373b7ed492c74c0
sharath <sharathc443@gmail.com>
2017-10-27 22:54:57 -0500

    little change in eventhandler

--- BMICal/About.aspx.cs
+++ BMICal/About.aspx.cs
@@ -13,16 +13,27 @@ namespace BMICal
         
         protected void btnBMICalculator_Click(object sender, EventArgs e)
         {
-            double HeightInMts = ((Convert.ToInt32(txtFeet.Text)) * 12 + (Convert.ToInt32(txtInch.Text))) * 0.0254;
-            double Weight = Convert.ToInt32(txtWeight.Text);
-            double result = (Weight / Math.Pow(HeightInMts, 2.0));
-            if (result > 24 && result < 30)
-                lblResult.Text = result.ToString() + " OverWeight";
-            if (result > 18 && result < 24)
-                lblResult.Text = result.ToString() + " Normal Weight";
-            if (result > 30)
-                lblResult.Text = result.ToString() + " Obese";
+            try
+            {
+                
+                double HeightInMts = ((Convert.ToInt32(txtFeet.Text)) * 12 + (Convert.ToInt32(txtInch.Text))) * 0.0254;
+                double Weight = Convert.ToInt32(txtWeight.Text);
+                double result = (Weight / Math.Pow(HeightInMts, 2.0));
+                if (result > 24 && result < 30)
+                    lblResult.Text = result.ToString() + " OverWeight";
+                if (result > 18 && result < 24)
+                    lblResult.Text = result.ToString() + " Normal Weight";
+                if (result > 30)
+                    lblResult.Text = result.ToString() + " Obese";
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text= ex.Message;
+            }
+            finally
+            {
 
+            }
         }
     }
 }
